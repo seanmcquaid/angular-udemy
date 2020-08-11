2af7ec61b41b14a5d9ef104ce059d8d089ae202f
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-10 20:58:07 -0400

    learned abour data binding, string interpolation and event binding

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
