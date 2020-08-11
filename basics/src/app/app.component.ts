10e978427613c8ed166458d49e90db81bb55e91a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-10 20:49:12 -0400

    similar to templates, I learned about adding css to styles

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
 })
 export class AppComponent {
   name: string;
