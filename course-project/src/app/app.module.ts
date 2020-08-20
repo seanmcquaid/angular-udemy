3e4cb46ec0e40d9a08dd8d064a1e0027956d65bd
seanmcquaid <sean.mcquaid2@verizon.net>
2020-08-19 21:07:45 -0400

    set up services and started initial use of recipe service

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,6 +27,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeService,
+    ShoppingListService,
   ],
   imports: [
     BrowserModule,
